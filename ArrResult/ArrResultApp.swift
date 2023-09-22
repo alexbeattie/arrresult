af2cb283fd43328d81dbaa17aa2ce73e067315ae
Alex Beattie <artisanbranding@me.com>
2023-09-22 15:34:57 -0700

    Initial Commit

---
+++ ArrResult/ArrResultApp.swift
@@ -0,0 +1,17 @@
+//
+//  ArrResultApp.swift
+//  ArrResult
+//
+//  Created by Alex Beattie on 9/22/23.
+//
+
+import SwiftUI
+
+@main
+struct ArrResultApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
