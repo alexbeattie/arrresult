5d50c6b3551a24e27c8708bfb9a1a6b2ed8174cf
Alex Beattie <artisanbranding@me.com>
2023-09-22 16:01:33 -0700

    finally

--- ArrResult/ContentView.swift
+++ ArrResult/ContentView.swift
@@ -7,15 +7,66 @@
 
 import SwiftUI
 
+struct Listing: Codable {
+    let value: [Value]
+}
+struct Value: Codable {
+    var BuyerAgentEmail: String?
+    var ClosePrice: Int?
+    var CoListAgentFirstName: String
+    var CoListAgentLastName: String
+    var Latitude: Float?
+    var Longitude: Float?
+    var ListPrice: Int?
+    var BedroomsTotal: Int?
+    var LotSizeAcres: Double?
+    var MlsStatus: String
+    var OffMarketDate: String?
+    var OnMarketDate: String?
+    var PendingTimestamp: String?
+    var Media: [Media]
+    var ListingKey: String
+}
+struct Media: Codable {
+    var MediaCategory: String
+    var MediaURL: String?
+}
+
+
 struct ContentView: View {
+    @State private var results = [Value]()
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        List(results, id:\.ListingKey) { item in
+            VStack(alignment: .leading) {
+                Text(item.CoListAgentFirstName)
+                    .font(.headline)
+                Text(item.CoListAgentLastName)
+            }
         }
-        .padding()
+        .task {
+            await loadData()
+            
+        }
+    .padding()
+    }
+    func loadData() async {
+        guard let url = URL(string: "http://lireadgroup.com/sparkData.json") else {
+            print("Invalid URL")
+            return
+        }
+        
+        do {
+            let (data, _) = try await URLSession.shared.data(from: url)
+            print(data)
+            if let decodedResponse = try? JSONDecoder().decode(Listing.self, from: data) {
+                results = decodedResponse.value
+            }
+            print(results)
+            // more code to come
+        } catch {
+            print("Invalid data")
+        }
+
     }
 }
 
